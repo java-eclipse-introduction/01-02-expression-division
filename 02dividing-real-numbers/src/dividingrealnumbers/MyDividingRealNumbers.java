3cc242acc4146b99dbf25c86a8cf0699857a493e
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-21 20:52:44 +0100

    DR.01 Osztás valós számmal

--- 02dividing-real-numbers/src/dividingrealnumbers/MyDividingRealNumbers.java
+++ 02dividing-real-numbers/src/dividingrealnumbers/MyDividingRealNumbers.java
@@ -5,6 +5,8 @@ public class MyDividingRealNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// DR.01 Osztás valós számmal
+		// Feladat: Írjon programot az ax+b=0 elsõfokú egyenlet megoldására a valós számok halmazán.
 	}
 
 }
