66cfb879cd49b31b8dec07befdb09198d574416b
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-21 21:57:49 +0100

    DWR.01 Adott n darab tojás, amit 20 férőhelyes tojás tartóba kell elpakolni.

--- 03-division-with-remainder/src/divisionwithremainder/MyDivisionWithRemainder.java
+++ 03-division-with-remainder/src/divisionwithremainder/MyDivisionWithRemainder.java
@@ -5,6 +5,8 @@ public class MyDivisionWithRemainder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// DWR.01 Adott n darab tojás, amit 20 férõhelyes tojás tartóba kell elpakolni. 
+		//		 Mennyi tartót lesz telepakolva, és az utolsó tojástartóba hány üres hely lesz?		
 	}
 
 }
