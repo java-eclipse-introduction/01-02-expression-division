80d8f7d679cc813dc2e1c4f41630b1a6f8bcb670
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-21 20:36:58 +0100

    DI.01 Osztás egész számmal

--- 01dividing-integers/src/dividingintegers/MyDividingIntegers.java
+++ 01dividing-integers/src/dividingintegers/MyDividingIntegers.java
@@ -5,6 +5,8 @@ public class MyDividingIntegers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		// DI.01 Osztás egész számmal
+		// Feladat: Írjon programot az ax+b=0 elsõfokú egyenlet megoldására az egész számok halmazán.
 	}
 
 }
